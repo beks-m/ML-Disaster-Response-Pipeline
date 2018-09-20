34fca6aaad81444f3e5bcef4cc3ef1bc31fd2b09
beks-m <beksultan.mambetov@gmail.com>
2018-09-20 16:42:34 +0000

    ETL implemented in process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,6 +1,7 @@
 import sys
 import pandas as pd
 from sqlalchemy import create_engine
+import numpy as np
 
 def load_data(messages_filepath, categories_filepath):
     messages = pd.read_csv(messages_filepath)
@@ -20,13 +21,13 @@ def load_data(messages_filepath, categories_filepath):
     return df
 
 def clean_data(df):
+    df.replace([np.inf, -np.inf], np.nan, inplace=True)
+    df.fillna(0, inplace=True)
     return df.drop_duplicates()
-
 
 def save_data(df, database_filename):
     engine = create_engine('sqlite:///' + database_filename)
     df.to_sql('table', engine, index=False)
-
 
 def main():
     if len(sys.argv) == 4:
@@ -36,10 +37,12 @@ def main():
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
         df = load_data(messages_filepath, categories_filepath)
-        print(df.head())
 
         print('Cleaning data...')
         df = clean_data(df)
+        
+        print('First five rows:')
+        print(df.head())
         
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
@@ -53,7 +56,6 @@ def main():
               'to as the third argument. \n\nExample: python process_data.py '\
               'disaster_messages.csv disaster_categories.csv '\
               'DisasterResponse.db')
-
 
 if __name__ == '__main__':
     main()
